f6134cd58214319aa81bef087674fc06385e15d5
Wen-Hung , Chang <bearada1975@gmail.com>
2021-03-15 22:26:03 +0800

    bumped version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="cv4t",
-    version="0.0.1",
+    version="0.0.2",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
     description="Computer Vision wrapper for Teenagers",
