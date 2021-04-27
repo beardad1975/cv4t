c41ead0bb4d1563e98c43773dbd9c9c44aae743a
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2021-04-28 00:14:23 +0800

    bumped version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="cv4t",
-    version="0.0.2",
+    version="0.0.3",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
     description="Computer Vision wrapper for Teenagers",
@@ -16,6 +16,7 @@ setuptools.setup(
     platforms=["Windows"],
     python_requires=">=3.5",
     packages=['cv4t','視覺模組'],
+    package_data={'cv4t': ['models/*']},
     install_requires = ['opencv-python>=4.4.0.44', 'Pillow>=7.2.0', 
                     'numpy>=1.19.2', 'mss>=6.0.0'],
         classifiers=[
