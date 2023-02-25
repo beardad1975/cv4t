7e22ae2c4452807e7ae6f943913a80c0dda6a718
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2023-02-25 22:47:00 +0800

    add 2 points transformation

--- cv4t/draw_lib.py
+++ cv4t/draw_lib.py
@@ -123,8 +123,10 @@ def blit_alpha_img(img, alpha_img, pos):
     alpha_img_height, alpha_img_width = alpha_img.shape[0], alpha_img.shape[1]
 
     # calculate top-left position
-    x = int(pos[0]) - alpha_img_width//2
-    y = int(pos[1]) - alpha_img_height//2
+    # x = int(pos[0]) - alpha_img_width//2
+    # y = int(pos[1]) - alpha_img_height//2
+    x = int(pos[0])
+    y = int(pos[1])
 
     #check range
     if not  0 <= x < img_width or not  0 <= y < img_height  :
@@ -163,3 +165,120 @@ def blit_alpha_img(img, alpha_img, pos):
     #print(img_blit)
     
     return img
+
+###############
+def makeRotateMatrix(angle):
+    cost = np.cos(np.deg2rad(angle))
+    sint = np.sin(np.deg2rad(angle))
+    mat = np.array([[cost, sint, 0],
+                   [-sint, cost, 0],
+                   [0, 0, 1]], dtype=np.float32)
+    return mat
+
+def makeTranslateMatrix(dx, dy):
+    mat = np.array([[1, 0, dx],
+                   [0, 1, dy],
+                   [0, 0, 1]], dtype=np.float32)
+    return mat
+
+def makeScaleMatrix(scaleX, scaleY):
+    mat = np.array([[scaleX, 0, 0],
+                   [0, scaleY, 0],
+                   [0, 0, 1]], dtype=np.float32)
+    return mat
+
+def calculateRotationAngle(a,b):
+    """ return rotation angle from vector a to vector b, in degrees.
+    Args:
+        a : np.array vector. format (x,y)
+        b : np.array vector. format (x,y)
+    Returns:
+        angle [float]: degrees. 0~360
+    """
+    # y axis reverse
+#     a[1] = -a[1]
+#     b[1] = -b[1]
+    
+    unit_vector_1 = a / np.linalg.norm(a)
+    unit_vector_2 = b / np.linalg.norm(b)
+    dot_product = np.dot(unit_vector_1, unit_vector_2)
+    angle = np.arccos(dot_product)
+    angle = angle/ np.pi * 180
+    c = np.cross(b,a)
+    #print('cross: ', c,  end=" ")
+    if c<0:
+#         angle +=180
+        angle = - angle
+    
+    return angle
+
+
+def transform_png_image(ori_img, ori_left_pt, ori_right_pt,
+                         dst_img, dst_pt1, dst_pt2):
+    height, width, channels = dst_img.shape
+    
+    # make sure ori pt1 is the left one
+#     if ori_left_pt[0] > ori_right_pt[0]:
+#         print('exchange ori pt1 pt2')
+#         ori_left_pt, ori_right_pt = ori_right_pt, ori_left_pt
+
+    # make sure  pt1 pt2 cant be  same x 
+#     if ori_left_pt[0] == ori_right_pt[0]:
+#         raise Exception("x of left pt cant be x of right pt")
+
+    # calculate vector
+    ori_x_vector = ori_right_pt[0] - ori_left_pt[0]
+    ori_y_vector = ori_right_pt[1] - ori_left_pt[1]
+    dst_x_vector = dst_pt2[0] - dst_pt1[0]
+    dst_y_vector = dst_pt2[1] - dst_pt1[1]
+
+    if ori_left_pt == ori_right_pt or dst_pt1 == dst_pt2:
+        raise Exception(" 來源或是目標的點1、2不能相同")
+
+
+    # determine source alpha
+    if channels == 4 :
+        has_alpha = True
+        b, g, r, alpha = cv2.split(ori_img)
+        ori_img = cv2.merge((b, g, r))
+
+    else :
+        has_alpha = False
+
+
+
+    # calculate scale ratio
+    ori_length = np.sqrt(abs(ori_x_vector)**2 +
+                           abs(ori_y_vector)**2)
+    dst_length = np.sqrt(abs(dst_x_vector)**2 +
+                           abs(dst_y_vector)**2)
+    scale_ratio = dst_length / ori_length
+    #print('scale ratio: ', scale_ratio)
+
+    # calculate angle
+    rotation_angle = calculateRotationAngle(
+                           np.array([ori_x_vector, ori_y_vector],dtype=np.float32),
+                           np.array([dst_x_vector, dst_y_vector],dtype=np.float32))
+    #print('rotation angle: ', rotation_angle)
+    
+
+
+    to_ori_M = makeTranslateMatrix(-ori_left_pt[0], -ori_left_pt[1])
+    rotate_M = makeRotateMatrix(rotation_angle)
+    
+    scale_M = makeScaleMatrix(scale_ratio, scale_ratio)
+    to_dst_M = makeTranslateMatrix(dst_pt1[0], dst_pt1[1])
+
+    M = np.matmul(rotate_M, to_ori_M)
+    M = np.matmul(scale_M, M)
+    M = np.matmul(to_dst_M, M)
+
+    affine_img = cv2.warpAffine(ori_img, M[:2, :], (width, height))
+
+    if has_alpha:
+        affine_alpha = cv2.warpAffine(alpha, M[:2, :], (width, height))
+        b, g, r = cv2.split(affine_img)
+        affine_img = cv2.merge((b, g, r, affine_alpha))
+
+
+    return affine_img
